34877903815cf20c65eb1b823657a9225e2dd2c4
Senne Wancour <senne.wancour1@gmail.com>
2021-01-03 14:31:14 +0100

    fixing mobile

--- scripts/main.js
+++ scripts/main.js
@@ -4,12 +4,14 @@ const randomValues = () => {
   document.getElementById('colourbox').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
   document.getElementById('colourbox').style.height = `${Math.floor(Math.random() * 20) + 40}vh`;
   document.getElementById('colourbox').style.top = `${Math.floor(Math.random() * 50) + 100}px`;
+  document.getElementById('colourbox').style.width = `${Math.floor(Math.random() * 20) + 40}%`;
+  document.getElementById('colourbox').style.right = `${Math.floor(Math.random() * 100)}px`;
 }
-
-document.getElementById('welcomeLogo').addEventListener('click', () => {
-  randomValues();
-})
 
 window.addEventListener('load', () => {
   randomValues();
 });
+
+window.setInterval(() => {
+  randomValues();
+}, 1500);
