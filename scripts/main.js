128c7fcb58ad3c9bafe6a720c8e9d3502274507e
nmdgent-sennwanc <senne.wancour@student.arteveldehs.be>
2021-01-03 15:06:16 +0100

    fixed navigation on smaller screens

--- scripts/main.js
+++ scripts/main.js
@@ -3,7 +3,7 @@ const colors = ['#bddbf2', '#91bed4', ' #ffba8d', '#f58c42'];
 const randomValues = () => {
   document.getElementById('colourbox').style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
   document.getElementById('colourbox').style.height = `${Math.floor(Math.random() * 20) + 40}vh`;
-  document.getElementById('colourbox').style.top = `${Math.floor(Math.random() * 50) + 100}px`;
+  document.getElementById('colourbox').style.top = `${Math.floor(Math.random() * 20) + 60}px`;
   document.getElementById('colourbox').style.width = `${Math.floor(Math.random() * 20) + 40}%`;
   document.getElementById('colourbox').style.right = `${Math.floor(Math.random() * 100)}px`;
 }
