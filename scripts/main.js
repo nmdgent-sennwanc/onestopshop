3dcbf14428a4bf019f0904298a601a912a7131be
Senne Wancour <senne.wancour1@gmail.com>
2021-01-03 20:53:52 +0100

    fixing mobile

--- scripts/main.js
+++ scripts/main.js
@@ -8,8 +8,29 @@ const randomValues = () => {
   document.getElementById('colourbox').style.right = `${Math.floor(Math.random() * 100)}px`;
 }
 
+let slideIndex = 0;
+
+const showSlides = () =>{
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000); 
+}
+
+
 window.addEventListener('load', () => {
   randomValues();
+  showSlides();
 });
 
 window.setInterval(() => {
